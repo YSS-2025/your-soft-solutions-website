e05f3d889ad3c82d79912e874e731de5ac29e259
SquishyMona <kotonefemc@gmail.com>
2025-02-26 13:17:03 -0500

    fixed some errors and contact page

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
     const { email, name, message } = await req.json()
     const confirmationMessage = `
         <div> 
-        <p style="text-align: center;"><span style="font-family: georgia, palatino, serif; font-size: 11pt;"><a title="Your Soft Solutions" href="https://www.yoursoftsolutions.com" rel="noopener noreferrer"><img src="blob:https://mail.hostinger.com/c145a421-ec0f-4e32-9d59-d209470efdfa" width="150" height="132" /></a></span></p>
+        <p style="text-align: center;"><span style="font-family: georgia, palatino, serif; font-size: 11pt;"><a title="Your Soft Solutions" href="https://www.yoursoftsolutions.com" rel="noopener noreferrer"><img src="blob:https://rmt7nuk8iqe5gdom.public.blob.vercel-storage.com/Logo%20Only%20Small-Zksas6jmh98Oq8ENoIcWuTnfNGDjYv.png" width="150" height="132" /></a></span></p>
         <table style="border-collapse: collapse; width: 95%; height: 83px; background-color: #ecf0f1; border-color: #95a5a6; border-style: solid; margin-left: auto; margin-right: auto;" border="0">
         <tbody>
         <tr style="height: 42px;">
